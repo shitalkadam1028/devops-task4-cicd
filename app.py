01a142d396f99b85f560d175513049855bb41aff
shitalkadam1028 <shitalkadam1028@gmail.com>
2026-07-11 09:32:49 +0000

    chnaged data from return statement

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Task 3 - Multi-stage Docker + Distroless"
+    return "Hello from Task 4 - CI/CD Pipeline using GitHub Actions, Docker & Amazon ECR"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
