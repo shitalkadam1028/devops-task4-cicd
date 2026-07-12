4caad5046f1a19abb64af9a95f30e99736ce6852
shitalkadam1028 <shitalkadam1028@gmail.com>
2026-07-12 18:47:34 +0000

    Read message from environment variables

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+MESSAGE = os.getenv("APP_MESSAGE", "Default Message")
+
+@app.route("/")
 def home():
-    return "Hello from Task 4 - CI/CD Pipeline using GitHub Actions, Docker & Amazon ECR"
+    return MESSAGE
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
